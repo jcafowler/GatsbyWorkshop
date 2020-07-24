e839ed989bdabca7755e554aaeded6835d9b24b5
Jay Fowler <jcafowler@gmail.com>
2020-07-24 18:58:08 -0400

    Remove avatar to redeploy

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react"
+
+const BioContent = () => (
+  <Fragment>
+    Words by Jay Fowler
+    <br/>
+    I'm a FullStack developer!
+  </Fragment>
+)
+
+export default BioContent
