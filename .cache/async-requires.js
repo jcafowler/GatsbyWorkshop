1a7300260feef51e52cdb616165686d194f47535
Jay Fowler <jcafowler@gmail.com>
2020-07-24 18:28:13 -0400

    Install Gatsby Theme Blog, make post, and add an avatar

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,6 +3,8 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---node-modules-gatsby-theme-blog-core-src-templates-post-query-js": () => import("./../../node_modules/gatsby-theme-blog-core/src/templates/post-query.js" /* webpackChunkName: "component---node-modules-gatsby-theme-blog-core-src-templates-post-query-js" */),
+  "component---node-modules-gatsby-theme-blog-core-src-templates-posts-query-js": () => import("./../../node_modules/gatsby-theme-blog-core/src/templates/posts-query.js" /* webpackChunkName: "component---node-modules-gatsby-theme-blog-core-src-templates-posts-query-js" */),
   "component---src-pages-about-mdx": () => import("./../../src/pages/about.mdx" /* webpackChunkName: "component---src-pages-about-mdx" */),
   "component---src-pages-index-js": () => import("./../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,8 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/javonfowler/PursuitCore/Extra/GatsbyWorkshop/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-theme-blog-core-src-templates-post-query-js": hot(preferDefault(require("/Users/javonfowler/PursuitCore/Extra/GatsbyWorkshop/node_modules/gatsby-theme-blog-core/src/templates/post-query.js"))),
+  "component---node-modules-gatsby-theme-blog-core-src-templates-posts-query-js": hot(preferDefault(require("/Users/javonfowler/PursuitCore/Extra/GatsbyWorkshop/node_modules/gatsby-theme-blog-core/src/templates/posts-query.js"))),
   "component---src-pages-about-mdx": hot(preferDefault(require("/Users/javonfowler/PursuitCore/Extra/GatsbyWorkshop/src/pages/about.mdx"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/javonfowler/PursuitCore/Extra/GatsbyWorkshop/src/pages/index.js")))
 }
